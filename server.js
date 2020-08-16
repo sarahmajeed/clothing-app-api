756e51ba2dd3070ae442076f0086d0195ee5769a
Sarahmdawood <sarahmajeed368@gmail.com>
2020-08-16 21:20:47 +0500

    :Adding more routes

--- server.js
+++ server.js
@@ -11,33 +11,69 @@ app.use(cors());
 
 
 app.get('/women/kurtis', (req, res) => {
-  db.select('kurtiimg', 'tag', 'kurtiprice').from('kurtis')
+  db.select('kurtiimg', 'tag', 'kurtiprice', 'kurtiid').from('kurtis')
     .then(data => res.send(data))
     .catch(err => console.log(err))
 })
 app.get('/women/trousers', (req, res) => {
-  db.select('trouserimg', 'tag', 'trouserprice').from('trousers')
+  db.select('trouserimg', 'tag', 'trouserprice', 'trouserid').from('trousers')
     .then(data => res.send(data))
     .catch(err => console.log(err))
 })
 app.get('/men/shirts', (req, res) => {
-  db.select('shirtimg', 'tag', 'shirtprice').from('shirts')
+  db.select('shirtimg', 'tag', 'shirtprice', 'shirtid').from('shirts')
     .then(data => res.send(data))
     .catch(err => console.log(err))
 })
 app.get('/men/pants', (req, res) => {
-  db.select('pantimg', 'tag', 'pantprice').from('pants')
+  db.select('pantimg', 'tag', 'pantprice', 'pantid').from('pants')
     .then(data => res.send(data))
     .catch(err => console.log(err))
 })
 app.get('/men/kurta', (req, res) => {
-  db.select('kurtaimg', 'tag', 'kurtaprice').from('kurtas')
+  db.select('kurtaimg', 'tag', 'kurtaprice', 'kurtaid').from('kurtas')
     .then(data => res.send(data))
     .catch(err => console.log(err))
 })
 app.get('/accessories', (req, res) => {
-  db.select('accimg', 'tag', 'accprice').from('accessories')
+  db.select('accimg', 'tag', 'accprice', 'accid').from('accessories')
     .then(data => res.send(data))
+    .catch(err => console.log(err))
+})
+app.get('/women/kurtis/:id', (req, res) => {
+  console.log(req.params)
+  db.select('color', 'kurtiimg', 'tag', 'kurtiprice', 'kurtiid', 'description').from('kurtis').where({ kurtiid: req.params.id })
+    .then(data => { res.send(data[0]) })
+    .catch(err => console.log(err))
+})
+app.get('/women/trousers/:id', (req, res) => {
+  console.log(req.params)
+  db.select('color', 'trouserimg', 'tag', 'trouserprice', 'trouserid', 'description').from('trousers').where({ trouserid: req.params.id })
+    .then(data => { res.send(data[0]) })
+    .catch(err => console.log(err))
+})
+app.get('/men/shirts/:id', (req, res) => {
+  console.log(req.params)
+  db.select('color', 'shirtimg', 'tag', 'shirtprice', 'shirtid', 'description').from('shirts').where({ shirtid: req.params.id })
+    .then(data => { res.send(data[0]) })
+    .catch(err => console.log(err))
+})
+app.get('/men/kurta/:id', (req, res) => {
+  console.log(req.params)
+  db.select('color', 'kurtaimg', 'tag', 'kurtaprice', 'kurtaid', 'description').from('kurtas').where({ kurtaid: req.params.id })
+    .then(data => { res.send(data[0]) })
+    .catch(err => console.log(err))
+})
+app.get('/men/pants/:id', (req, res) => {
+  console.log(req.params)
+  db.select('color', 'pantimg', 'tag', 'pantprice', 'pantid', 'description').from('pants').where({ pantid: req.params.id })
+    .then(data => { res.send(data[0]) })
+    .catch(err => console.log(err))
+})
+app.get('/accessories/:id', (req, res) => {
+  console.log(req.params)
+  db.select('color', 'accimg', 'tag', 'accprice', 'accid', 'description').from('accessories').where({ accid: req.params.id })
+    .then(data => { res.send(data[0]) })
     .catch(err => console.log(err))
 })
 app.listen(5000, () => console.log('listening to port 5000'));
