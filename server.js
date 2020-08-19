f7a849bc69fdb3dc21ea65418f2c8cc57b9bc54f
Sarahmdawood <sarahmajeed368@gmail.com>
2020-08-19 19:58:09 +0500

    Adding route

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
 app.post("/signup", (req, res) => {
   const { name, email, password } = req.body;
   const hash = bcrypt.hashSync(password, saltRounds);
